67e8b2fe33ec2c8bb55600f3cdbea16435b0b2e5
devincd <505259926@qq.com>
2019-05-13 23:33:20 +0800

    009

--- 001_twoSum/main.go
+++ 001_twoSum/main.go
@@ -1,15 +1,8 @@
 /**
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
-
-你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
-
-示例:
-
-给定 nums = [2, 7, 11, 15], target = 9
-
-因为 nums[0] + nums[1] = 2 + 7 = 9
-所以返回 [0, 1]
-
+官方解法:
+一遍哈希表(采用以空间换取速度的方式)
+在进行迭代并将元素插入到表中的同时，我们还会回过头来检查表中是否已经存在当前元素所对应的目标元素。如果它存在
+那我们已经找到了对应解，并立即返回
  */
 
 package main
@@ -27,9 +20,11 @@ func main() {
 func twoSum(nums []int, target int) []int {
 	numsMap := make(map[int]int)
 	for key, value := range nums {
+		//(1)检查当前元素是否在哈希表中已经存在
 		if i, ok := numsMap[value]; ok {
 			return []int{i, key}
 		}
+		//(2)将目标值-当前元素值存入到哈希表中去
 		numsMap[target - value] = key
 	}
 	return nil
---
+++ 009_palindrome/main.go
@@ -0,0 +1,19 @@
+/**
+回文数
+官方解法:
+
+ */
+package main
+
+
+func main() {
+
+}
+
+func isPalindrome(x int) bool {
+	//(1)负整数肯定都不是回文数
+	if x < 0 {
+		return false
+	}
+	return true
+}
