2e51abfc7c95962d06759581c0af8f57556501d5
devincd <505259926@qq.com>
2019-05-20 23:26:00 +0800

    add leetcode

--- 009_palindrome/main.go
+++ 009_palindrome/main.go
@@ -5,15 +5,25 @@
  */
 package main
 
+import "fmt"
 
 func main() {
-
+	fmt.Println(isPalindrome(121))
 }
 
 func isPalindrome(x int) bool {
-	//(1)负整数肯定都不是回文数
-	if x < 0 {
+	// 特殊情况
+	// (1)当x < 0时, x不是回文数
+	// (2)同样地，如果数字的最后一位是0，为了使该数字为回文，则第一位也应该是0
+	if x < 0 ||  (x % 10 == 0 && x != 0) {
 		return false
 	}
-	return true
+	
+	var revertedNumber int
+	for x > revertedNumber {
+		revertedNumber = revertedNumber * 10 + x %10
+		x = x /10
+	}
+	// 当数字长度为奇数时，我们可以通过revertedNumber/10去除处于中位的数字
+	return x == revertedNumber || x == revertedNumber/10
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/devincd/leetcode
+
+go 1.12
